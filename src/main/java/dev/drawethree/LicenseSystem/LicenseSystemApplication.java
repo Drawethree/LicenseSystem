a8c45877a36218d3fa56ddd3a42e2d393462fe30
Drawethree <janci.kluka@gmail.com>
2021-11-23 21:10:22 +0100

    Initial project setup via Spring initializr

---
+++ src/main/java/dev/drawethree/LicenseSystem/LicenseSystemApplication.java
@@ -0,0 +1,13 @@
+package dev.drawethree.LicenseSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicenseSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicenseSystemApplication.class, args);
+	}
+
+}
